807dba73b20dffa5d3d7a44d28324cea0c8d852d
maple! <averyrudelphe@gmail.com>
2024-07-07 14:08:18 +0200

    unstub stringizer

--- src/stringizer.h
+++ src/stringizer.h
@@ -36,4 +36,25 @@ namespace Stringizer {
 	}
 #endif
 	QString stringize(const lcf::rpg::EventCommand& com);
+
+	// Common stringize operations
+	QString varName(int id);
+	QString switchName(int id);
+	QString itemName(int id);
+	QString heroName(int id);
+	QString className(int id);
+	QString stateName(int id);
+	QString skillName(int id);
+	QString battleCommandName(int id);
+	QString animationName(int id);
+	QString conditionName(int id);
+	QString eventName(int id);
+	QString commonEventName(int id);
+	QString troopName(int id);
+	QString terrainName(int id);
+	QString mapName(int id);
+	QString direction(int id);
+	QString moveCommand(int id);
+	QString eraseTransitionName(int id);
+	QString showTransitionName(int id);
 }
